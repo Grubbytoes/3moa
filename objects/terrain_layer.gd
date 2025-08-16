73f1b8e130dde58ff45a10d5281433ec6544b856
Hugo <hugoaaabbott@gmail.com>
2025-08-16 14:13:49 +0100

    Reworked terrain layer

--- objects/terrain_layer.gd
+++ objects/terrain_layer.gd
@@ -1,21 +1,41 @@
 class_name TerrainLayer
 extends TileMapLayer
 
+const TILE_SIZE = 32
+
+var tile_damage := {}
 
 func hit_tile_at(point_of_collision: Vector2):
 	var hit_tile_coords = Vector2i(
-		floor((point_of_collision.x - position.x) / 16),
-		floor((point_of_collision.y - position.y) / 16)
+		floor((point_of_collision.x - position.x) / TILE_SIZE),
+		floor((point_of_collision.y - position.y) / TILE_SIZE)
 	)
 	var hit_tile := get_cell_tile_data(hit_tile_coords)
 
 	if !hit_tile:
 		return
-
-	# I think this is a rough way of doing it but we'll see
+	
 	var hardness: int = hit_tile.get_custom_data("hardness")
 
 	if 0 >= hardness:
 		erase_cell(hit_tile_coords)
 	else:
-		set_cell(hit_tile_coords, 2, Vector2i(0, hardness - 1))
+		push_warning("hardness not fully implemented or tested")
+		# 1. see if the tile is already in tile_damage, if not add it with hardness -1
+		var damaged_hardness = tile_damage.get(hit_tile_coords)
+
+		if damaged_hardness == null:
+			tile_damage.set(hit_tile_coords, hardness-1)
+		elif 0 < damaged_hardness:
+			tile_damage.set(hit_tile_coords, damaged_hardness-1)
+		else:
+			erase_cell(hit_tile_coords)
+			tile_damage.erase(hit_tile_coords)
+
+
+func reset_tile_damage(key: Vector2i) -> bool:
+	return tile_damage.erase(key)
+
+
+class TileDamageEntry:
+	pass
---
+++ todo.md
@@ -0,0 +1,11 @@
+## TODO
+* start menu
+* end of game logic
+* enemy bots
+* enemy placement
+
+## IN PROGRESS
+* basic procedural generation
+* advanced terrain destruction
+
+## DONE
